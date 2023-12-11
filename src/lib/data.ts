96a14bb011b9658e1b86c41ccb92cc73cbe34158
Nicholas St. Germain <nick@cajun.pro>
2023-12-10 22:49:09 -0600

    add gitignore, 2 links, missing env var, and idea files

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -27,7 +27,19 @@ export const links: Link[] = [
   {
     name: "Dex",
     value: "dex",
-    description: "Trader Joe MAGIC/AVAX.",
+    description: "Get MAGICK at Trader Joe",
     link: "https://traderjoexyz.com/avalanche/trade?outputCurrency=0x9a8e0217cd870783c3f2317985c57bf570969153"
+  },
+  {
+    name: "MAGICK/USDC LP",
+    value: "magick-usdc-liquidity",
+    description: "Trader Joe MAGICK/USDC Liquidity Pool",
+    link: "https://traderjoexyz.com/avalanche/pool/v1/0x9a8e0217cd870783c3f2317985c57bf570969153/0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"
+  },
+  {
+    name: "MAGICK/AVAX LP",
+    value: "magick-avax-liquidity",
+    description: "Trader Joe MAGICK/AVAX Liquidity Pool",
+    link: "https://traderjoexyz.com/avalanche/pool/v1/0x9a8e0217cd870783c3f2317985c57bf570969153/AVAX"
   }
 ]
